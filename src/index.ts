0f03bd522c4339ca5fb591a24fb6dd278c3abdd9
Ilia Kamilov <iliakmlv@gmail.com>
2024-01-26 11:16:27 +0200

    auth router & base middlewares

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,18 @@ import Express from 'express'
 import Http from 'http'
 import Config from './config'
 import Database from './config/db'
+import CookieParser from 'cookie-parser'
+import Cors from 'cors'
 
 const app: Express.Application = Express()
+
+app.use(CookieParser())
+app.use(Express.json())
+app.use(Cors({
+    origin: ['http://localhost:3000','http://10.0.0.8:3000'],
+    credentials: true
+}))
+
 const server: Http.Server = Http.createServer(app)
 
 Database.connect()
